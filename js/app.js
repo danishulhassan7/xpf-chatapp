19378f6c112ab237f70621475b24e4ab64830ec0
danishulhassan7 <danishulhassan7@gmail.com>
2020-09-19 14:36:33 +0500

    7: Done with the right side-2 of Chatbox

---
+++ js/app.js
@@ -0,0 +1,5 @@
+
+function startChat(id) {
+    document.getElementById('chatPanel').removeAttribute('style'); 
+    document.getElementById('startTemplate').setAttribute('style','display:none');
+}
