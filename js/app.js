c374d43823536da5dcd2f18541b6ea8405045afa
danishulhassan7 <danishulhassan7@gmail.com>
2020-09-19 19:27:53 +0500

    9: Function to show the firend list on small screen

--- js/app.js
+++ js/app.js
@@ -3,3 +3,11 @@ function startChat(id) {
     document.getElementById('chatPanel').removeAttribute('style'); 
     document.getElementById('startTemplate').setAttribute('style','display:none');
 }
+
+
+// on small screen to show the friend chat list 
+
+function showChatList() {
+    document.getElementById('side-1').classList.remove('d-none','d-md-block');
+    document.getElementById('chatPanel').classList.add('d-none');
+}
