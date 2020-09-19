c8b6597a19fca17a8132bc8d9d1500de516dce46
danishulhassan7 <danishulhassan7@gmail.com>
2020-09-19 19:53:47 +0500

    11: Done with the basic funcionalities of the web-app

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 function startChat(id) {
     document.getElementById('chatPanel').removeAttribute('style'); 
     document.getElementById('startTemplate').setAttribute('style','display:none');
+    hideChatList();
 }
 
 
