7a809703a1455078b336244318673c23a8f56ced
danishulhassan7 <danishulhassan7@gmail.com>
2020-09-20 10:07:25 +0500

    12: Done with the sending messages fom our site

--- js/app.js
+++ js/app.js
@@ -19,3 +19,32 @@ function hideChatList() {
     document.getElementById('side-1').classList.add('d-none','d-md-block');
     document.getElementById('chatPanel').classList.remove('d-none');
 }
+
+
+//Sending a message using enter key
+
+function onKeyDown() {
+    document.addEventListener('keydown',function (key) {
+        if(key.which === 13) {
+            sendMessage();
+        }
+    });
+}
+
+function sendMessage() {
+    var message = ` <div class="row justify-content-end">
+    <div class="col-sm-8 col-8 col-md-7">
+        <p class="sent-msg float-right">
+        ${document.getElementById('textMessage').value}
+            <span class="small time">8.35 PM</span>
+        </p>
+    </div>
+    <div class="col-2 col-sm-1 col-md-1">
+        <img src="images/profiles/avatar.png" alt="profile" class="chat-pic img-fluid">
+    </div>
+</div>` 
+
+    document.getElementById('messages').innerHTML += message;
+    document.getElementById('textMessage').value = '';
+    document.getElementById('textMessage').focus();
+}
